d84843b1c231733022a97333ee26520d835601f9
ZC <335134701@qq.com>
2019-07-13 09:16:38 +0800

    SomeChange

--- MyOperation/Properties/AssemblyInfo.cs
+++ MyOperation/Properties/AssemblyInfo.cs
@@ -14,6 +14,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+
+//自定义配置文件 ConfigFile :配置名;Watch:监听
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = @"Configuration\Log4net.config", Watch = true)]
+
+
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 //请将此类型的 ComVisible 特性设置为 true。
